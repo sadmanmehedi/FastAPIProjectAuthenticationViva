c5314cf6d8a530deea4b6c82f1f69dddcbe63011
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-06-20 15:09:47 +0600

    Project Complete

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from fastapi import FastAPI,Body,Depends
 from app.model import PostSchema
 from app.model import PostSchema,UserLoginSchema,UserSchema
 from app.auth.jwt_handler import signJWT
+from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi import HTTPException
 
+
+# MongoDB connection settings
+MONGODB_URL = "mongodb+srv://admin:admin@cluster0.lb7qjfv.mongodb.net/Authentication"
+DATABASE_NAME = "Authentication"
+
+# Create a MongoDB client
+client = AsyncIOMotorClient("mongodb+srv://admin:admin@cluster0.lb7qjfv.mongodb.net/Authentication")
+
+# Get a reference to the database
+db = client["Authentication"]
 
 posts=[
     {
@@ -36,53 +48,48 @@ def greet():
 
 
 #2Get Posts
-@app.get("/ posts",tags=["posts"])
-def get_posts():
-    return{"data":posts}
+@app.get("/posts", tags=["posts"])
+async def get_posts():
+    posts = await db.posts.find().to_list(length=None)
+    return {"data": posts}
 
 
 #3Get single Posts aboout ID
-@app.get("/posts/{id}",tags=["posts"])
-def get_one_post(id: int):
-    if id>len(posts):
-        return{
-            "error":"Posts with this ID does not exist!"
-        }
-    
-    for post in posts:
-        if post["id"]==id:
-            return{
-                "data":post
-            }
-        
+@app.get("/posts/{id}", tags=["posts"])
+async def get_one_post(id: int):
+    post = await db.posts.find_one({"id": id})
+    if post:
+        return {"data": post}
+    return {"error": "Post with this ID does not exist!"}
 
 #4 Post a blog Post[A handler for creating a Post]
-@app.post("/posts",tags=["posts"])
-def add_post(post:PostSchema):
-    post.id=len(posts)+1
-    posts.append(post.dict())
-    return{
-        "info":"Post Added!"
-    }
+@app.post("/posts", tags=["posts"])
+async def add_post(post: PostSchema):
+    post_dict = post.dict()
+    post_dict["id"] = len(posts) + 1
+    await db.posts.insert_one(post_dict)
+    return {"info": "Post Added!"}
   
 
   #5User Signup[Create a new User ]
-@app.post("/user/signup",tags=["user"])
-def user_signup(user: UserSchema=Body(default=None)):
-    users.append(user)
+@app.post("/user/signup", tags=["user"])
+async def user_signup(user: UserSchema = Body(default=None)):
+    users_collection = db.users
+    existing_user = await users_collection.find_one({"email": user.email})
+    if existing_user:
+        raise HTTPException(status_code=409, detail="User already exists")
+
+    await users_collection.insert_one(user.dict())
     return signJWT(user.email)
 
-def check_user(data:UserLoginSchema):
-    for user in users:
-        if user.email==data.email and user.password==data.password:
-            return True
-        return False
-    
-@app.post("/user.login",tags=["user"])
-def user_login(user:UserLoginSchema=Body(default=None)):
-    if check_user(user):
+async def check_user(data: UserLoginSchema):
+    users_collection = db.users
+    user = await users_collection.find_one({"email": data.email, "password": data.password})
+    return user is not None
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(default=None)):
+    if await check_user(user):
         return signJWT(user.email)
     else:
-        return {
-            "error":"Invalid Login Details!"
-        }
+        return {"error": "Invalid Login Details!"}
