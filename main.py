5477bf4b902a9528c60183dad40fae7faa7622b3
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-06-20 14:16:09 +0600

    Bearer file e problem ache

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import fastapi
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,Body,Depends
 from app.model import PostSchema
 from app.model import PostSchema,UserLoginSchema,UserSchema
 from app.auth.jwt_handler import signJWT
@@ -65,3 +65,24 @@ def add_post(post:PostSchema):
         "info":"Post Added!"
     }
   
+
+  #5User Signup[Create a new User ]
+@app.post("/user/signup",tags=["user"])
+def user_signup(user: UserSchema=Body(default=None)):
+    users.append(user)
+    return signJWT(user.email)
+
+def check_user(data:UserLoginSchema):
+    for user in users:
+        if user.email==data.email and user.password==data.password:
+            return True
+        return False
+    
+@app.post("/user.login",tags=["user"])
+def user_login(user:UserLoginSchema=Body(default=None)):
+    if check_user(user):
+        return signJWT(user.email)
+    else:
+        return {
+            "error":"Invalid Login Details!"
+        }
